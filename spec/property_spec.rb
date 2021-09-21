32064ccd93c7e63de2fe97d57680f6e200af1c7e
tj <tomisinjoseph3@gmail.com>
2021-09-21 17:07:30 +0100

    updated .all test

--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -1,8 +1,19 @@
 require 'property'
 
 describe Property do
+  describe '.all' do
+    it 'lists all the properties in table' do
+      connection = PG.connect(dbname: 'bnb_test')
+      connection.exec("INSERT INTO properties (name, description, price, availability) VALUES ('House1','this is a description', 5, true);")
+      connection.exec("INSERT INTO properties (name, description, price, availability) VALUES ('House2','this is another description', 10, false);")
 
-  it 'returns true' do
-  expect(subject.method).to eq true
+      result = Property.all
+      expect(result.length).to eq 2
+      expect(result.first.name).to eq 'House1' 
+      expect(result.first.description).to eq 'this is a description'
+      expect(result.first.price).to eq 5
+      expect(result.first.availability).to eq true
+      # expect(result.first.id).to eq 
+    end
   end
 end
