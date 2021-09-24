243ec7a417c75e362f51bbb2ba9ce04ec6402e89
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-24 09:58:08 +0100

    new feature test for unavailable property

--- spec/feature/unavailable_property_spec.rb
+++ spec/feature/unavailable_property_spec.rb
@@ -1,12 +1,12 @@
 feature 'cannot see unavailable property' do
 
   scenario 'cannot see book' do
-
+    user = User.add_new_user(email: 'user@dnb.co.uk', password: "password")
     PGDatabase.insert_property(name: 'House999', description:'this is a description', price:  5, availability: true, user_id: user.id)
-
     PGDatabase.insert_property(name: 'House12322', description:'this is a description', price:  5, availability: false, user_id: user.id)
     visit('/')
-    expect()
+    expect(page).to have_content('House999')
+    expect(page).not_to have_content('House12322')
   end
 
 end
